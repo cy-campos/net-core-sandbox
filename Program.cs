c4071bc03e8059dcd96d7442569b40e870a301e2
cy-campos <cy.campos1983@gmail.com>
2018-06-22 06:58:09 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
